cb00552dc6845b6bafd96f6c75af81b0c3e522db
Charlie McMackin <charliemac@gmail.com>
2018-02-03 04:39:28 -0600

    Get island assets displaying for island components

--- src/hawaii_quiz/core.cljs
+++ src/hawaii_quiz/core.cljs
@@ -1,13 +1,41 @@
 (ns hawaii-quiz.core
-    (:require
-      [reagent.core :as r]))
+  (:require
+   [reagent.core :as r]))
+
+(def island-names [
+                   "NIIHAU"
+                   "KAUAI"
+                   "OAHU"
+                   "MOLOKAI"
+                   "LANAI"
+                   "MAUI"
+                   "HAWAII"
+                   "KAHOOLAWE"
+                   ])
 
 ;; -------------------------
 ;; Views
+(defn score-board []
+  )
+
+(defn island [name]
+  (let [normal-name (clojure.string/lower-case name) ]
+    [:div {:class normal-name}
+     [:img {:src (str "img/" normal-name ".svg")}]]))
+
+(defn islands []
+  (map (fn [name] ^{:key name} [island name])
+       island-names))
+
+(defn button-list []
+  [:ul
+   (map (fn [item] ^{:key item} [:li [:button item]])
+        island-names)])
 
 (defn home-page []
-  [:div [:h2 "Welcome to Reagent"]])
-
+  [:div.App
+   [:div.ocean (islands)]
+   [:div.dashboard [button-list]]])
 ;; -------------------------
 ;; Initialize app
 
